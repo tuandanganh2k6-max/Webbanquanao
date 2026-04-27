4c6c373dab4d39be08ee8d3f2c10c569a47614c7
Tuấn <tuandanganh2k6-max@gmail.com>
2026-04-27 16:59:56 +0700

    Correct Render API link

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,3 @@
-const API_BASE_URL = 'http://localhost:5000'; 
-// Khi deploy lên Render, hãy đổi thành link Render của bạn:
-// const API_BASE_URL = 'https://webbanquanao-backend.onrender.com';
+const API_BASE_URL = 'https://webbanquanao-backend.onrender.com';
 
 export default API_BASE_URL;
